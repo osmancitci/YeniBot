85dcbe48f2087b88ec9dbd3b36af6df1fa6692ed
Glitch (awesome-marvelous-cobweb) <none>
2024-02-01 22:27:52 +0000

    🚴🐛 Checkpoint ./commands/premium.js:32654060/9952 ./config.json:32654060/25

--- commands/premium.js
+++ commands/premium.js
@@ -1,112 +1,152 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
-const fs = require('fs');
-const config = require('../config.json');
-const CatLoggr = require('cat-loggr');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+const fs = require("fs");
+const config = require("../config.json");
+const CatLoggr = require("cat-loggr");
 
 const log = new CatLoggr();
 const generated = new Set();
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('ozel')
-        .setDescription('Stok varsa belirli bir hizmet oluşturun')
-        .addStringOption(option =>
-            option.setName('service')
-                .setDescription('Oluşturulacak hizmetin adı')
-                .setRequired(true)),
+  data: new SlashCommandBuilder()
+    .setName("ozel")
+    .setDescription("Stok varsa belirli bir hizmet oluşturun")
+    .addStringOption((option) =>
+      option
+        .setName("service")
+        .setDescription("Oluşturulacak hizmetin adı")
+        .setRequired(true)
+    ),
 
-    async execute(interaction) {
-        const service = interaction.options.getString('service');
-        const member = interaction.member;
+  async execute(interaction) {
+    const service = interaction.options.getString("service");
+    const member = interaction.member;
 
-        // Check if the channel where the command was used is the generator channel
-        if (interaction.channelId !== config.premiumChannel) {
-            const wrongChannelEmbed = new MessageEmbed()
-                .setColor(config.color.red)
-                .setTitle('Yanlış komut kullanımı!')
-                .setDescription(`Bu kanalda \`/ozel\` komutunu kullanamazsınız! <#${config.premiumChannel}>'da deneyin!`)
-                .setFooter(interaction.user.tag, interaction.user.displayAvatarURL({ dynamic: true, size: 64 }))
-                .setTimestamp();
+    // Check if the channel where the command was used is the generator channel
+    if (interaction.channelId !== config.premiumChannel) {
+      const wrongChannelEmbed = new MessageEmbed()
+        .setColor(config.color.red)
+        .setTitle("Yanlış komut kullanımı!")
+        .setDescription(
+          `Bu kanalda \`/ozel\` komutunu kullanamazsınız! <#${config.premiumChannel}>'da deneyin!`
+        )
+        .setFooter(
+          interaction.user.tag,
+          interaction.user.displayAvatarURL({ dynamic: true, size: 64 })
+        )
+        .setTimestamp();
 
-            return interaction.reply({ embeds: [wrongChannelEmbed], ephemeral: true });
-        }
+      return interaction.reply({
+        embeds: [wrongChannelEmbed],
+        ephemeral: true,
+      });
+    }
 
-        // Check if the user has cooldown on the command
-        if (generated.has(member.id)) {
-            const cooldownEmbed = new MessageEmbed()
-                .setColor(config.color.red)
-                .setTitle('Sakin ol!')
-                .setDescription(`Bu komutu tekrar çalıştırmadan önce lütfen **${config.premiumCooldown}** saniye bekleyin!`)
-                .setFooter(interaction.user.tag, interaction.user.displayAvatarURL({ dynamic: true, size: 64 }))
-                .setTimestamp();
+    // Check if the user has cooldown on the command
+    if (generated.has(member.id)) {
+      const cooldownEmbed = new MessageEmbed()
+        .setColor(config.color.red)
+        .setTitle("Sakin ol!")
+        .setDescription(
+          `Bu komutu tekrar çalıştırmadan önce lütfen **${config.premiumCooldown}** saniye bekleyin!`
+        )
+        .setFooter(
+          interaction.user.tag,
+          interaction.user.displayAvatarURL({ dynamic: true, size: 64 })
+        )
+        .setTimestamp();
 
-            return interaction.reply({ embeds: [cooldownEmbed], ephemeral: true });
-        }
+      return interaction.reply({ embeds: [cooldownEmbed], ephemeral: true });
+    }
 
-        // File path to find the given service
-        const filePath = `${__dirname}/../premium/${service}.txt`;
+    // File path to find the given service
+    const filePath = `${__dirname}/../premium/${service}.txt`;
 
-        // Read the service file
-        fs.readFile(filePath, 'utf-8', (error, data) => {
-            if (error) {
-                const notFoundEmbed = new MessageEmbed()
-                    .setColor(config.color.red)
-                    .setTitle('Jeneratör Hatası!')
-                    .setDescription(`\`${service}\` hizmeti mevcut değil!`)
-                    .setFooter(interaction.user.tag, interaction.user.displayAvatarURL({ dynamic: true, size: 64 }))
-                    .setTimestamp();
+    // Read the service file
+    fs.readFile(filePath, "utf-8", (error, data) => {
+      if (error) {
+        const notFoundEmbed = new MessageEmbed()
+          .setColor(config.color.red)
+          .setTitle("Jeneratör Hatası!")
+          .setDescription(`\`${service}\` hizmeti mevcut değil!`)
+          .setFooter(
+            interaction.user.tag,
+            interaction.user.displayAvatarURL({ dynamic: true, size: 64 })
+          )
+          .setTimestamp();
 
-                return interaction.reply({ embeds: [notFoundEmbed], ephemeral: true });
-            }
+        return interaction.reply({ embeds: [notFoundEmbed], ephemeral: true });
+      }
 
-            const lines = data.split(/\r?\n/);
+      const lines = data.split(/\r?\n/);
 
-            if (lines.length <= 1) {
-                const emptyServiceEmbed = new MessageEmbed()
-                    .setColor(config.color.red)
-                    .setTitle('Jeneratör Hatası!')
-                    .setDescription(`\`${service}\` hizmeti boş!`)
-                    .setFooter(interaction.user.tag, interaction.user.displayAvatarURL({ dynamic: true, size: 64 }))
-                    .setTimestamp();
+      if (lines.length <= 1) {
+        const emptyServiceEmbed = new MessageEmbed()
+          .setColor(config.color.red)
+          .setTitle("Jeneratör Hatası!")
+          .setDescription(`\`${service}\` hizmeti boş!`)
+          .setFooter(
+            interaction.user.tag,
+            interaction.user.displayAvatarURL({ dynamic: true, size: 64 })
+          )
+          .setTimestamp();
 
-                return interaction.reply({ embeds: [emptyServiceEmbed], ephemeral: true });
-            }
-
-            const generatedAccount = lines[0];
-
-            // Remove the redeemed account line
-            lines.shift();
-            const updatedData = lines.join('\n');
-
-            // Write the updated data back to the file
-            fs.writeFile(filePath, updatedData, (writeError) => {
-                if (writeError) {
-                    log.error(writeError);
-                    return interaction.reply('Hesabı kullanırken bir hata oluştu.');
-                }
-
-                const embedMessage = new MessageEmbed()
-                    .setColor(config.color.green)
-                    .setTitle('Özel hesap oluşturuldu')
-                    .setFooter(interaction.user.tag, interaction.user.displayAvatarURL({ dynamic: true, size: 64 }))
-                    .setDescription('🙏 Özel üye olduğunuz için çok teşekkür ederiz! \n 🌟 Desteğiniz bizim için dünyalara bedel! 💖😊')
-                    .addField('Servis', `\`\`\`${service[0].toUpperCase()}${service.slice(1).toLowerCase()}\`\`\``, true)
-                    .addField('Hesap', `\`\`\`${generatedAccount}\`\`\``, true)
-                    .setImage(config.banner)
-                    .setTimestamp();
-
-                member.send({ embeds: [embedMessage] })
-                    .catch(error => console.error(`Yerleştirme mesajı gönderilirken hata oluştu: ${error}`));
-                interaction.reply({
-                    content: `**DM ${member} numaranızı kontrol edin!** __Mesajı almazsanız, lütfen özel hesabınızın kilidini açın!__`,
-                });
-
-                generated.add(member.id);
-                setTimeout(() => {
-                    generated.delete(member.id);
-                }, config.premiumCooldown * 1000);
-            });
+        return interaction.reply({
+          embeds: [emptyServiceEmbed],
+          ephemeral: true,
         });
-    },
+      }
+
+      const generatedAccount = lines[0];
+
+      // Remove the redeemed account line
+      lines.shift();
+      const updatedData = lines.join("\n");
+
+      // Write the updated data back to the file
+      fs.writeFile(filePath, updatedData, (writeError) => {
+        if (writeError) {
+          log.error(writeError);
+          return interaction.reply("Hesabı kullanırken bir hata oluştu.");
+        }
+
+        const embedMessage = new MessageEmbed()
+          .setColor(config.color.green)
+          .setTitle("Özel hesap oluşturuldu")
+          .setFooter(
+            interaction.user.tag,
+            interaction.user.displayAvatarURL({ dynamic: true, size: 64 })
+          )
+          .setDescription(
+            "🙏 Özel üye olduğunuz için çok teşekkür ederiz! \n 🌟 Desteğiniz bizim için dünyalara bedel! 💖😊"
+          )
+          .addField(
+            "Servis",
+            `\`\`\`${service[0].toUpperCase()}${service
+              .slice(1)
+              .toLowerCase()}\`\`\``,
+            true
+          )
+          .addField("Hesap", `\`\`\`${generatedAccount}\`\`\``, true)
+          .setImage(config.banner)
+          .setTimestamp();
+
+        member
+          .send({ embeds: [embedMessage] })
+          .catch((error) =>
+            console.error(
+              `Yerleştirme mesajı gönderilirken hata oluştu: ${error}`
+            )
+          );
+        interaction.reply({
+          content: `**DM ${member} numaranızı kontrol edin!** __Mesajı almazsanız, lütfen özel hesabınızın kilidini açın!__`,
+        });
+
+        generated.add(member.id);
+        setTimeout(() => {
+          generated.delete(member.id);
+        }, config.premiumCooldown * 1000);
+      });
+    });
+  },
 };
